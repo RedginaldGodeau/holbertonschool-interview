199691aba495275ea5f2f009026d5570ec74d98f
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2024-08-09 11:07:43 +0200

    add prod

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+    """ can it unlock all ? """
+    array = []
+
+    k = 0 if len(boxes) == 1 else 1
+
+    for init in boxes[0]:
+        if init not in array:
+            array.append(init)
+
+    for elem in array:
+        try:
+            for keys in boxes[elem]:
+                if keys not in array and keys != 0:
+                    array.append(keys)
+        except IndexError:
+            continue
+
+    array.sort()
+    if not (k + len(array) == len(boxes)):
+        for i, chiffre in enumerate(array):
+            chiffre = chiffre - 1
+            if i != chiffre:
+                return False
+        return True
+    else:
+        return True
